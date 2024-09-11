a354970c1643e5af962fa25a3155a829b9da5c1f
SobczakL <lucassobczak3@gmail.com>
2024-09-10 22:24:55 -0400

    added chakra arch

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import overrides from "./styles/theme/index.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<ChakraProvider theme={overrides}>
+			<App />
+		</ChakraProvider>
+	</StrictMode>,
+);
---
+++ src/styles/theme/styles.js
@@ -0,0 +1,24 @@
+const styles = {
+    fonts: {
+        latoR: "latoR",
+        latoSB: "latoSB",
+    },
+    fontSizes: {
+        mobile: {
+            header: "31px",
+            subheader: "45px",
+            body: {
+                lg: "20px",
+                md: "16px",
+                sm: "14px",
+            },
+        },
+    },
+    lineHeights: {
+    },
+    colors: {
+        mainBg: "#232121",
+    },
+};
+
+export default styles;
