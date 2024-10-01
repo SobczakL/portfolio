960ac259482f530b373f0a0a4f1653938ae2915f
SobczakL <lucassobczak3@gmail.com>
2024-09-30 20:51:28 -0400

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Start from "./pages/Start"
+import Home from "./pages/Home"
 
 function App() {
 	return (
 		<Routes>
             <Route path="/" element={<Start/>}/>
+            <Route path="/home" element={<Home/>}/>
         </Routes>
 	);
 }
---
+++ src/components/Tile.jsx
@@ -0,0 +1,5 @@
+export default function Tile(){
+    return(
+        <p>Tile</p>
+    )
+}
