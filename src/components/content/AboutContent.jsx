49411ad5e4b30ae6dc8afe08d480b83e2f7266e6
SobczakL <lucassobczak3@gmail.com>
2024-10-09 21:47:01 -0400

    test

--- src/components/content/AboutContent.jsx
+++ src/components/content/AboutContent.jsx
@@ -1,11 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Text, VStack } from "@chakra-ui/react";
 
-export default function AboutContent({content}) {
-    return (
-        <Box fontSize={["body.sm", "body.md", "body.lg"]}>
-            <p>
-                {content.name}
-            </p>
-        </Box>
-    );
+export default function AboutContent({ content }) {
+    return <></>;
 }
