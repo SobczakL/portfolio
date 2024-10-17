d877044bf715da43830cbb0135cc09e895c6f72c
Lucas Sobczak <36972429+SobczakL@users.noreply.github.com>
2024-10-16 23:34:21 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home"
 function App() {
 	return (
 		<Routes>
+            
             <Route path="/" element={<Start/>}/>
             <Route path="/home" element={<Home/>}/>
         </Routes>
