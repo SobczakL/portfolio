43fa6a3a41a5b81ff33786e45c07da888c590a9c
Lucas sobczak <lucassobczak@Lucass-MacBook-Pro.local>
2024-10-23 21:52:13 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home"
 function App() {
 	return (
 		<Routes>
-            
             <Route path="/" element={<Start/>}/>
             <Route path="/home" element={<Home/>}/>
         </Routes>
