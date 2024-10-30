7e4f9add94e3229549e0508f9d19a7ea089e8eb0
Lucas sobczak <lucassobczak3@gmail.com>
2024-10-29 20:24:20 -0400

    test upstream

--- src/components/content/AboutContent.jsx
+++ src/components/content/AboutContent.jsx
@@ -2,5 +2,9 @@ import { useState } from "react";
 import { Box, Text, VStack } from "@chakra-ui/react";
 
 export default function AboutContent({ content }) {
-    return <></>;
+    return(
+        <>
+            <p></p>
+        </>
+    )
 }
